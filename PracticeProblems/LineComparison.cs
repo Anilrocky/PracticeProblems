51ab121a153417df764e683047f8595fafb8885a
Anilrocky <anilkumarac96@gmail.com>
2023-03-11 12:19:29 +0530

    [Anil] Add:Line Comparison of two lines

---
+++ PracticeProblems/LineComparison.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblems
+{
+    public class LineComparison
+    {
+        public double Length()
+        {
+            Console.WriteLine("Input x1");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input y1");
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input x2");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input y2");
+            int y2 = Convert.ToInt32(Console.ReadLine());
+            double length = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            Console.WriteLine(length);
+            return length;
+        }
+        public void Equality(double line1, double line2)
+        {
+            if (line1.Equals(line2))
+                Console.WriteLine("Lines are equal");
+            else
+                Console.WriteLine("Lines are not equal");
+        }
+        public void Compare(double line1, double line2)
+        {
+            if (line1.CompareTo(line2) > 0)
+                Console.WriteLine("line1 is greater than line2");
+            if (line1.CompareTo(line2) < 0)
+                Console.WriteLine("line2 is greater than line1");
+            if (line1.CompareTo(line2) == 0)
+                Console.WriteLine("Both lines are equal");
+        }
+    }
+}
--- PracticeProblems/Program.cs
+++ PracticeProblems/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace PracticeProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to practice problems");             
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Choose an option to perform \n1.Line Comparison \n2.Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        LineComparison lineComparison1 = new LineComparison();
+                        double line1 = lineComparison1.Length();
+                        LineComparison lineComparison2 = new LineComparison();
+                        double line2 = lineComparison2.Length();
+                        lineComparison2.Equality(line1, line2);
+                        lineComparison2.Compare(line1, line2);
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            }           
+        }
+    }
+}
